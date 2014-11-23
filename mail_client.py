f1f875f4a5001d50baa3a8c8f3754ba290f69fb0
Jonah Rosenberg <jonahrosenberg@spotmac111.attlocal.net>
2014-11-22 18:18:48 -0600

    Add test mail_client

---
+++ mail_client.py
@@ -0,0 +1,30 @@
+import smtplib
+import base64
+
+SENDER = base64.b64decode("c2V0LnRpbWVzLmFwcGxpY2F0aW9uQGdtYWlsLmNvbQ==")
+PASSWORD = base64.b64decode("JDN0LXRpbWVz")
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+
+if __name__ == '__main__':
+  recipient = "JonahRRosenberg@gmail.com"
+  subject = "Gmail SMTP Test"
+  body = "HERRO"
+
+  headers = "\r\n".join(["From: " + "Set Times",
+             "Subject: " + subject,
+             "To: " + recipient,
+             "MIME-Version: 1.0",
+             "Content-Type: text/html"])
+
+  session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+  session.ehlo()
+  session.starttls()
+  session.ehlo
+  session.login(SENDER, PASSWORD)
+   
+  session.sendmail(SENDER, recipient, headers + "\r\n\r\n" + body)
+  print "Sent!"
+  session.quit()
+  print "Quit"
+
